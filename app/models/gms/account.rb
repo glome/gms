0bf74f05ec71ee53fe9e04324ce68015f90c1fd3
Ferenc Szekely <ferenc.szekely@gmail.com>
2014-06-17 11:53:19 +0300

    improve validations

--- app/models/gms/account.rb
+++ app/models/gms/account.rb
@@ -1,14 +1,18 @@
 module Gms
   class Account < ActiveRecord::Base
-    #attr_accessible :glomeid, :user_id, :name, :password, :domain, :resource, :alias
+    attr_accessible :glomeid, :domain
     attr_accessor :glomeid, :password
 
     validates :name, :uniqueness => { :case_sensitive => false }
+    validates :domain, :presence => true
+    validates :user_id, :presence => true
 
     belongs_to :user, class_name: "User"
 
     before_save :preset
-    after_save :register_jabber
+    # TODO: not yet working; get sidekiq workers working in modules
+    # actually call the lib/gms/xmpp.rb lib create method from this
+    #after_save :register_jabber
 
     private
 
@@ -19,9 +23,9 @@ module Gms
         found = true
         until not found
           self.name = SecureRandom.hex(8)
-          self.password = self.name
           found = Account.where(name: self.name).take
         end
+        self.password = self.name
       end
     end
 
@@ -30,7 +34,7 @@ module Gms
         :name => self.name,
         :alias => self.alias,
         :resource => self.resource,
-        :domain => self.resource,
+        :domain => self.domain,
         :password => self.password
       }
       logger.debug 'Call EjabberdRegister async: ' + struct.inspect
